7a278a5440c8222bde61aff0373d670da483c603
AdaGundog <147503106+AdaGundog@users.noreply.github.com>
2025-05-14 15:58:33 +0300

    adadadadayusuf

--- Assets/BuildingPanel.cs
+++ Assets/BuildingPanel.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 public class BuildingPanel : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
+    public MonoBehaviour tilePlacer; // Drag your script component here in the Inspector
+
 
     public void TogglePanel()
     {
         if (panel != null)
         {
             panel.SetActive(!panel.activeSelf);
+        }
+    }
+
+    public void ToggleTargetScript()
+    {
+        if (tilePlacer != null)
+        {
+            tilePlacer.enabled = !tilePlacer.enabled;
         }
     }
 }
